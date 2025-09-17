1c8ec77d2308ef8b100ded6c8a91f691d1ee177a
sabin <sabinkarki064@gmail.com>
2025-09-17 23:41:07 +0545

    added custom method

--- src/main/java/com/project/MediFlow/Repository/PatientRepository.java
+++ src/main/java/com/project/MediFlow/Repository/PatientRepository.java
@@ -3,6 +3,9 @@ package com.project.MediFlow.Repository;
 import com.project.MediFlow.Model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface PatientRepository extends JpaRepository<Patient,Long> {
 
+    Boolean existsByFirstAndLastName(String firstName, String lastName, LocalDate dob);
 }

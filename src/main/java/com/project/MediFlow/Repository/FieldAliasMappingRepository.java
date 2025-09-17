c65d9180a37abed3e95dcd8e01f6de2dd2d33a1c
sabin <sabinkarki064@gmail.com>
2025-09-17 23:40:26 +0545

    added method

--- src/main/java/com/project/MediFlow/Repository/FieldAliasMappingRepository.java
+++ src/main/java/com/project/MediFlow/Repository/FieldAliasMappingRepository.java
@@ -3,6 +3,11 @@ package com.project.MediFlow.Repository;
 import com.project.MediFlow.Model.FieldAliasMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FieldAliasMappingRepository extends JpaRepository<FieldAliasMapping,Long> {
 
+    static List<FieldAliasMapping> findByEntityType(String entityType) {
+        return List.of();
+    }
 }

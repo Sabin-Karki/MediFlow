a28fd30d8183c23dba06f891fd47dbc9d60dfa13
sabin <sabinkarki064@gmail.com>
2025-09-22 22:26:55 +0545

    added repo

--- src/main/java/com/project/MediFlow/Repository/DoctorRepository.java
+++ src/main/java/com/project/MediFlow/Repository/DoctorRepository.java
@@ -3,5 +3,9 @@ package com.project.MediFlow.Repository;
 import com.project.MediFlow.Model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+    Boolean existsByFirstNameAndLastName(String firstName, String lastName, LocalDate dob);
 }
+

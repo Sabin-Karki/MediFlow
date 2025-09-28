1f124e7df934077f07bace3658801affb198a485
sabin <sabinkarki064@gmail.com>
2025-09-28 13:27:31 +0545

    string to localdate

---
+++ src/main/java/com/project/MediFlow/DTO/DoctorDTO.java
@@ -0,0 +1,19 @@
+package com.project.MediFlow.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorDTO {
+private  String  externalId;
+private String firstName;
+private  String lastName;
+private LocalDate DOB;
+
+}

517fdfe65e501981d8fb5231d43a07f02268e7e6
sabin <sabinkarki064@gmail.com>
2025-10-05 22:29:08 +0545

    added Lab Result DTO

--- src/main/java/com/project/MediFlow/DTO/LabResultDTO.java
+++ src/main/java/com/project/MediFlow/DTO/LabResultDTO.java
@@ -4,7 +4,6 @@ package com.project.MediFlow.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 

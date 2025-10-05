48baccdad410d91db546c418e1e7aa27bca1e372
sabin <sabinkarki064@gmail.com>
2025-10-05 22:30:23 +0545

    added Ce

--- src/main/java/com/project/MediFlow/Model/ClinicalEncounter.java
+++ src/main/java/com/project/MediFlow/Model/ClinicalEncounter.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -38,7 +39,7 @@ public class ClinicalEncounter {
     @OneToMany(mappedBy = "encounter")
     private  List<Prescription> prescriptionList;
 
-    private LocalDateTime localDateTime;
+    private LocalDate date;
 
 
 }

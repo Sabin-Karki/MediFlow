8f8ea9839cb742a52a268e580cb1b075bcaba770
sabin <sabinkarki064@gmail.com>
2025-10-08 09:22:46 +0545

    implementation

--- src/main/java/com/project/MediFlow/Service/PipeLineServiceImpl.java
+++ src/main/java/com/project/MediFlow/Service/PipeLineServiceImpl.java
@@ -3,30 +3,43 @@ package com.project.MediFlow.Service;
 import com.project.MediFlow.Model.JobStatus;
 import com.project.MediFlow.Model.RawDataEvent;
 import com.project.MediFlow.Repository.RawDataEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 @Service
 public class PipeLineServiceImpl implements  PipeLineService {
 
     private final RawDataEventRepository rawDataEventRepository;
+  private  final AsyncProcessingService asyncProcessingService;
     private final String FILE_UPLOADS = "/home/kantwasright/Documents/uploads";
 
-    public PipeLineServiceImpl(RawDataEventRepository rawDataEventRepository) {
+
+    //so i really have intitally the list of instance of fileprocessor in list basically
+    //then while creating map im creating a collection of key-value of filetype->instance/implementation of fileprocessor,eg-Patient-PatientProcesoor,basic storage/collectiopn whatver.and then later i will retrieve the correct instance of the filetype using method .get(key) where key is filetype
+    public PipeLineServiceImpl(RawDataEventRepository rawDataEventRepository,AsyncProcessingService asyncProcessingService) {
         this.rawDataEventRepository = rawDataEventRepository;
+        this.asyncProcessingService=asyncProcessingService;
+
+
     }
 
     public RawDataEvent saveRawDataEvent(MultipartFile file, String fileType) throws IllegalArgumentException {
@@ -47,6 +60,7 @@ public class PipeLineServiceImpl implements  PipeLineService {
                 RawDataEvent rawDataEvent = new RawDataEvent(
                         file.getOriginalFilename(),
                         filePath.toString(),
+                        fileType,
                         LocalDateTime.now(),
                         JobStatus.RECEIVED
                 );
@@ -54,7 +68,7 @@ public class PipeLineServiceImpl implements  PipeLineService {
 
                 RawDataEvent savedRawDataEvent = rawDataEventRepository.save(rawDataEvent);
 
-                this.processPipeLine(savedRawDataEvent.getId());
+                asyncProcessingService.processPipeLine(savedRawDataEvent.getId());
                 return savedRawDataEvent;
             } catch (java.io.IOException e) {
                 System.err.println("Failed to save file" + e.getMessage());
@@ -63,40 +77,6 @@ public class PipeLineServiceImpl implements  PipeLineService {
 
         }
     }
-
-
-
-    @Async
-    public void processPipeLine(Long id) {
-        //monitor the database of raw data event and check the status,when it sees the status it changes received to processing,it then gets the file path and processes it,determins the type of file it is and then process it accordingly //after processing change the status code to completed//i want to use async processing not scheduled !!!
-        Optional<RawDataEvent> optionalEvent = rawDataEventRepository.findById(id);
-        if (optionalEvent.isPresent()) {
-            RawDataEvent rawDataEvent = optionalEvent.get();
-            //update status to processing after retriving the object
-            try {
-                rawDataEvent.setStatus(JobStatus.PROCESSING);
-                rawDataEventRepository.save(rawDataEvent);
-                if(rawDataEvent.getOriginalFileName().endsWith("csv")){
-                   processPatient(rawDataEvent);
-                }
-            } catch (Exception e) {
-                System.err.println("Failed to update status" + e.getMessage());
-
-            }
-
-
-        }
-    }
-
-    public void processPatient(RawDataEvent rawDataEvent){
-        //i need to see status of processing
-        if(rawDataEvent.getStatus()==JobStatus.PROCESSING){
-            // read the file
-            
-        }
-
-    }
-
 }
 
 

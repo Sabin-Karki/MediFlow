8a07d74b59dbaa363fd879c4ae1bf4167899e9f7
sabin <sabinkarki064@gmail.com>
2025-10-10 23:30:12 +0545

    added encounter repo

--- src/main/java/com/project/MediFlow/Repository/ClinicalEncounterRepository.java
+++ src/main/java/com/project/MediFlow/Repository/ClinicalEncounterRepository.java
@@ -5,9 +5,12 @@ import com.project.MediFlow.Model.Doctor;
 import com.project.MediFlow.Model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface ClinicialEncounterRepository extends JpaRepository<ClinicalEncounter,Long> {
-    Optional<ClinicalEncounter> findByDoctorAndPatientAndDate(Doctor doctor, Patient patient , LocalDateTime localDateTime);
+public interface ClinicalEncounterRepository extends JpaRepository<ClinicalEncounter,Long> {
+
+    Optional<ClinicalEncounter> findByPatientAndDate(Patient patient , LocalDate date);
+    Optional<ClinicalEncounter> findByDoctorAndPatientAndDate(Doctor doctor, Patient patient, LocalDate date);
 }

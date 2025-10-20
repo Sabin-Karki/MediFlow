3c37071f91b2e4fbca1d50f4cbc76873b77ee486
sabin <sabinkarki064@gmail.com>
2025-10-20 13:11:31 +0545

    repo for labresult

--- src/main/java/com/project/MediFlow/Repository/LabResultRepository.java
+++ src/main/java/com/project/MediFlow/Repository/LabResultRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LabResultRepository extends JpaRepository<LabResult,Long> {
 
+    boolean existsByExternalId(String externalId);
 }

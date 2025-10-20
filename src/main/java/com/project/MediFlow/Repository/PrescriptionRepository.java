5e8b25ff1f34b4b7ab50f19a7e5fb0c6267c34f8
sabin <sabinkarki064@gmail.com>
2025-10-20 13:12:34 +0545

    prescription repo

--- src/main/java/com/project/MediFlow/Repository/PrescriptionRepository.java
+++ src/main/java/com/project/MediFlow/Repository/PrescriptionRepository.java
@@ -4,4 +4,5 @@ import com.project.MediFlow.Model.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PrescriptionRepository extends JpaRepository<Prescription,Long> {
+  Boolean existsByExternalId(String externalId);
 }

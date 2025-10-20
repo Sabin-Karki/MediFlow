9edd5d5a6b5ca3ef9800ab3979097c8eb60f3654
sabin <sabinkarki064@gmail.com>
2025-10-20 13:11:51 +0545

    patient repo

--- src/main/java/com/project/MediFlow/Repository/PatientRepository.java
+++ src/main/java/com/project/MediFlow/Repository/PatientRepository.java
@@ -3,9 +3,9 @@ package com.project.MediFlow.Repository;
 import com.project.MediFlow.Model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-
 public interface PatientRepository extends JpaRepository<Patient,Long> {
 
-    Boolean existsByFirstAndLastName(String firstName, String lastName, LocalDate dob);
+    Boolean existsByFirstNameAndLastName(String firstName, String lastName);
+
+    Patient findByExternalId(String externalId);
 }
